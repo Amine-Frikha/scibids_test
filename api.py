64b828771dfdeaeadf93256124b5440ce7259dd5
Amine-Frikha <aminfrikha@outlook.com>
2022-06-30 20:06:58 +0200

    gey_by_id

--- api.py
+++ api.py
@@ -5,7 +5,6 @@ import db
 app = Flask(__name__)
 api = Api(app)
 
-
 class Documents(Resource):
     def get(self):
         try:
@@ -14,9 +13,21 @@ class Documents(Resource):
             return {'status': 500, 'errors': str(e)}, 500
 
         return {'status': 200, 'data': documents}, 200
+    
+
+class Document(Resource):
+    def get(self, id):
+        try:
+            documents = db.get_document_by_id(id)
+        except db.DatabaseError as e:
+            return {'status': 500, 'errors': str(e)}, 500
+
+        return {'status': 200, 'data': documents}, 200
 
 
-api.add_resource(Documents, '/documents')
+api.add_resource(Documents, '/documents/')
+api.add_resource(Document, '/documents/<id>')
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=7003)
--- db.py
+++ db.py
@@ -24,3 +24,17 @@ def get_documents():
             documents.append(add_headers_sqlite(d, cursor))
 
     return documents
+
+def get_document_by_id(id):
+    document = []
+    with sqlite3.connect("document.db") as conn:
+        try:
+            cursor = conn.execute('SELECT * FROM document WHERE id = {}'.format(id))
+            data = cursor.fetchall()
+        except sqlite3.Error as err:
+            raise DatabaseError(err.args[0])
+
+        for d in data:
+            document.append(add_headers_sqlite(d, cursor))
+
+    return document
